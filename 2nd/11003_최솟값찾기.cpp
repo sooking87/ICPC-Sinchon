d60bebe4fe4e3fbd42909377002c0e9797a73c4a
sooking87 <sksohn01@naver.com>
2024-07-29 17:03:34 +0900

    2nd

--- 2nd/11003_최솟값찾기.cpp
+++ 2nd/11003_최솟값찾기.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
-    
+    int n, l, temp;
+    cin >> n >> l;
+    vector<int> arr;
+    vector<int> res;
+    // 배열 입력받기
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    res.push_back(arr[0]);
+    res.push_back((arr[0] < arr[1] ? arr[0] : arr[1]));
+    // 인덱스 3부터 최소값찾기
+    for (int i = 0; i < n-l+1; i++) {
+        int min = arr[i];
+        for (int j = i + 1; j < i+l; j++) {
+            if (min > arr[j]) {
+                min = arr[j];
+            }
+        }
+        res.push_back(min);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << " ";
+    }
 }
---
+++ 2nd/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+res.push_back(arr[0]);
+    // res.push_back((arr[0] < arr[1] ? arr[0] : arr[1]));
+    // for (int i = 0; i < res.size(); i++) {
+    //     cout << res.at(i) << endl;
+    // }
